c2bd905c41a3641e8a380e2e50a6c4561ebca072
rinat <rinatsikunda@mail.tau.ac.il>
2021-08-04 18:56:23 +0300

    DB cahnges

--- server/src/database/querys.js
+++ server/src/database/querys.js
@@ -1,9 +1,9 @@
 export const queries = {
     getAllEmployees: "SELECT * FROM Employees",
-    addNewEmployee: "INSERT INTO employees (Name, Email, Mobile ,Department ,HireDate) VALUES (@Name, @Email, @Mobile ,@Department ,@HireDate)",
-    deleteEmployee: "DELETE FROM [employees].[dbo].[employees] WHERE Id = @Id",
-    getTotalEmployees: "SELECT COUNT(*) FROM [employees].[dbo].[employees]",
-    updateEmployeeById: "UPDATE [employees].[dbo].[employees] SET Name = @Name, Email = @Email, Mobile = @Mobile, Department = @Department WHERE Id = @id",
-    getEmployeeById: "SELECT * FROM [employees].[dbo].[employees] WHERE Id = @Id"
+    addNewEmployee: "INSERT INTO Employees (Name, Email, Mobile ,Department ,HireDate) VALUES (@Name, @Email, @Mobile ,@Department ,@HireDate)",
+    deleteEmployee: "DELETE FROM [Copmany].[dbo].[Employees] WHERE Id = @Id",
+    getTotalEmployees: "SELECT COUNT(*) FROM [Copmany].[dbo].[Employees]",
+    updateEmployeeById: "UPDATE [Copmany].[dbo].[Employees] SET Name = @Name, Email = @Email, Mobile = @Mobile, Department = @Department WHERE Id = @id",
+    getEmployeeById: "SELECT * FROM [Copmany].[dbo].[Employees] WHERE Id = @Id"
 
 };
